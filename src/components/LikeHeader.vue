0492438a620000a1be6980ecec4d518bccb49ecd
田村 海斗 <kaito.tamura@proptech.plus>
2020-11-06 16:12:13 +0900

    add new file

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <slot ></slot>
+</div>
+</template>
+
+<script>
+export default {
+   data(){
+     return{
+       user:{
+         firstName: 'Jack',
+         lastName: 'Donald'
+       }
+     };
+   }
+};
+</script>
+
